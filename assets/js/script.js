80f69360092ce82f1beb9db00c49e771be7693eb
Facundo Cristaldo <facundocristlado146@gmail.com>
2025-05-30 00:10:22 -0300

    Se agrego funcion flecha para llamar a la API

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+const urlDBZ = "https://dragonball-api.com/api/characters";
+const boton_mostrar = document.getElementById("btn-mostrar");
+const info = document.getElementById("contenedor-info");
+
+const fetchInfo = async (url) => {
+    try {
+        const respon = await fetch(url);
+
+        if (!respon.ok) {
+            throw new Error("Algo salió mal. Inténtelo más tarde :/");
+        }
+
+        const data = await respon.json();
+        return data;
+
+    } catch (error) {
+        console.log("Ocurrió un error:", error);
+    }
+}
+const conocerMas = async (id)=>{
+    try {
+        const respon = await fetch(`${urlDBZ}/${id}`);
+
+        if (!respon.ok) {
+            throw new Error("Algo salió mal. Inténtelo más tarde :/");
+        }
+
+        const data = await respon.json();
+
+        alert(data.description);
+
+    } catch (error) {
+        console.log("Ocurrió un error:", error);
+    }
+}
+
+boton_mostrar.addEventListener("click", async () => {
+    const personajes = await fetchInfo(urlDBZ);
+    const datapersonaje = personajes.items;
+
+    console.log(datapersonaje);
+
+    datapersonaje.forEach((personaje) => {
+        info.innerHTML += `
+        <div class="col-3" data-id=${personaje.id}>
+                <img 
+                class="card-img-top"    
+                src=${personaje.image}
+                />
+                <h2>${personaje.name}</h2>
+                <p>${personaje.race} / ${personaje.gender}</p>
+                <button class="btn-conocer-mas"> Conocer Más</button>
+        </div>
+
+        `;
+        
+    });
+})
+
+info.addEventListener("click",(e)=>{
+    if (e.target.classList.contains("btn-conocer-mas")) {
+    const cardPadre =  e.target.closest(".col-3");
+    const id = cardPadre.dataset.id
+    conocerMas(id);
+    }
+})
